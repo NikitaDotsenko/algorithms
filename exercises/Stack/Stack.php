b0eddc8597b92ec2e5fab6faa4e62a8be64da0f6
nikita.dotsenko <indahazzard@gmail.com>
2020-12-06 13:51:53 +0200

    Complete stack.

---
+++ exercises/Stack/Stack.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Exercises\Stack;
+
+/**
+ * Create a Stack.
+ *
+ * When stack is empty pop and peek methods should return null.
+ *
+ * @method void push(int $value)
+ * @method mixed|null pop()
+ * @method mixed|null peek()
+ * @example $stack = new Stack();
+ * $stack->push(1);
+ * $stack->push(2);
+ * $stack->peek() === 1;
+ * $stack->pop() === 1;
+ */
+final class Stack
+{
+    private $stack  = [];
+    private $cursor = 0;
+
+    /**
+     * Can be refactored to array_push;
+     *
+     * @param int $value
+     */
+    public function push(int $value): void
+    {
+        $this->stack[$this->cursor] = $value;
+        $this->cursor++;
+    }
+
+    /**
+     * Can be refactored to array_pop;
+     *
+     * @return int|null
+     */
+    public function pop(): ?int
+    {
+        if ($this->cursor > 0) {
+            $value = $this->stack[$this->cursor - 1];
+            unset($this->stack[$this->cursor]);
+
+            if ($this->cursor !== 0) {
+                $this->cursor--;
+            }
+
+            return $value;
+        }
+
+        return null;
+    }
+
+    /**
+     * Get last element.
+     *
+     * @return int|null
+     */
+    public function peek(): ?int
+    {
+        return $this->cursor === 0
+            ? null
+            : $this->stack[$this->cursor - 1];
+    }
+}
